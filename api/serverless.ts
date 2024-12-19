11d3e3c5135f9a8fa58232edc2893950d5cb6599
Maxim Kuznetsov <fixer40@bk.ru>
2024-12-19 20:59:31 +0700

    !

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,10 +1,6 @@
-import { handle } from 'hono/vercel';
+import { handle } from '@hono/node-server/vercel';
 import app from '../server';
 
 const handler = handle(app);
 
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+export default handler;
