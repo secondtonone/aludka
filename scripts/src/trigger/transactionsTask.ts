b90ab177dad56835a515d3d0305f26c3a2e4ebf0
Maxim Kuznetsov <fixer40@bk.ru>
2025-01-04 15:39:22 +0700

    Add transactions task

---
+++ scripts/src/trigger/transactionsTask.ts
@@ -0,0 +1,23 @@
+import { envvars, logger, schedules } from '@trigger.dev/sdk/v3';
+import { transactionsToDb } from '../transactionsToDb';
+
+export const transactionsTask = schedules.task({
+  id: 'transactions-task',
+  cron: '1 0 * * *',
+  maxDuration: 300,
+  run: async (payload, { ctx }) => {
+    const distanceInMs =
+      payload.timestamp.getTime() -
+      (payload.lastTimestamp ?? new Date()).getTime();
+
+    logger.log('Transactions task', { payload, distanceInMs });
+
+    const variables = await envvars.list(ctx.project.ref, ctx.environment.slug);
+
+    const env = variables.reduce((acc, variable) => ({ ...acc, [variable.name]: variable.value }), {} as Record<string, string>);
+
+    await transactionsToDb(env);
+
+    logger.log('Done.');
+  },
+});
